10af7427f9c86c16537895d9c65d2f1993b12215
al43hi58 <31436767+al43hi58@users.noreply.github.com>
2017-11-28 10:26:07 +0100

    LetterToNumber and CeaserCypher

--- CeaserCypher.py
+++ CeaserCypher.py
@@ -6,11 +6,33 @@
 # INSTRUCTIONS
 
 ##############################################################
-# This is where you asignment will be created
+# This is where you assignment will be created
 ##############################################################
+from pip._vendor.distlib.compat import raw_input
+Decode = raw_input("Decode? ")
+Encode = raw_input("Encode? ")
 alphabet="abcdefghijklmopqrstuvwxyz"
-Decode = input("decode")
+decode = raw_input("Type your sentence: ")
 
+def decoding():
     if Decode == "Y" or "Yes":
 
-    elif Decode == "N" or "No":
+        print("Unfinished")
+    if Decode == "N" or "No":
+        encoding()
+    else:
+        print("You did not say 'Yes 'or 'No'")
+        decoding()
+
+
+
+def encoding():
+    if Encode == "Y" or "Yes":
+        print("Unfinished")
+    elif Encode == "N" or "No":
+        decoding()
+    else:
+        print("You did not say 'Yes' or 'No'")
+        encoding()
+
+decoding()
--- LetterToNumber.py
+++ LetterToNumber.py
@@ -18,14 +18,20 @@ alphabetbig="ABCDEFGHIJKLMNOPQRSTUVWXYZ"
 alphabetsmall="abcdefghijklmnopqrstuvwxyz"
 # Ask the user to type in a letter
 letter=input("Please type a letter: ")
-
 # Find the position of the letter in the alphabet
 posbig=alphabetbig.find(letter)
 possmall=alphabetsmall.find(letter)
 # As the first position is 0, we need to add 1
 posbig += 1
 possmall += 1
+if posbig == 1 and possmall == 1:
+    posbig = "You "
+    possmall = "didn't type anything, please type any letter"
+if not letter.isalpha():
+    posbig = "Please "
+    possmall = "type a LETTER not anything else. "
+elif len(letter) > 1:
+    posbig = "Please "
+    possmall = "type ONE letter not any more. "
 #display the number back to the user
 print(posbig + possmall)
-
-
